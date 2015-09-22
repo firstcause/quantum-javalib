772f32b15e971e7dd45fef0f3e3dbd6cd8300ee4
Steven Marsh <sjm@tiger.quarknet>
2015-09-22 14:29:26 -0500

    rearranging to use master build script to allow dependencies to be built.

---
+++ JavaLibs/oshelper/src/main/java/com/denizensoft/oshelper/Crypter.java
@@ -0,0 +1,254 @@
+package com.denizensoft.oshelper;
+
+import android.util.Base64;
+import android.util.Log;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+
+/**
+ * Created by sjm on 1/28/2015.
+ */
+@SuppressWarnings("ALL")
+public class Crypter
+{
+
+	private static final String TAG = "Crypter";
+
+	//char set
+	private String mCharsetName = "UTF8";
+	//base mode
+	private int mBase64Mode = Base64.DEFAULT;
+	//type of aes key that will be created
+	private String mSecretKeyType = "PBKDF2WithHmacSHA1";
+	//value used for salting. can be anything
+	private String mSalt = "some_salt";
+	//length of key
+	private int mKeyLength = 128;
+
+	//number of times the password is hashed
+	private int mIterationCount = 256;
+
+	//main family of aes
+	private String mAlgorithm = "AES";
+
+	/**
+	 * Encrypt a {@link String}
+	 *
+	 * @param key  the {@link String} key
+	 * @param data the {@link String} to be encrypted
+	 * @return the encrypted {@link String} or <code>null</code> if occur some error
+	 */
+	public String encrypt(String key, String data)
+	{
+		if(key == null || data == null) return null;
+		try
+		{
+			SecretKey secretKey = getSecretKey(hashTheKey(key));
+			byte[] dataBytes = data.getBytes(mCharsetName);
+			Cipher cipher = Cipher.getInstance(mAlgorithm);
+			cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+			return Base64.encodeToString(cipher.doFinal(dataBytes), mBase64Mode);
+		}
+		catch(Exception e)
+		{
+			Log.e(TAG, e.toString());
+			return null;
+		}
+	}
+
+	/**
+	 * Decrypt a {@link string}
+	 *
+	 * @param key  the {@link String} key
+	 * @param data the {@link String} to be decrypted
+	 * @return the decrypted {@link String} or <code>null</code> if occur some error
+	 */
+	public String decrypt(String key, String data)
+	{
+		if(key == null || data == null) return null;
+		try
+		{
+			byte[] dataBytes = Base64.decode(data, mBase64Mode);
+			SecretKey secretKey = getSecretKey(hashTheKey(key));
+			Cipher cipher = Cipher.getInstance(mAlgorithm);
+			cipher.init(Cipher.DECRYPT_MODE, secretKey);
+			byte[] dataBytesDecrypted = (cipher.doFinal(dataBytes));
+			return new String(dataBytesDecrypted);
+		}
+		catch(Exception e)
+		{
+			Log.e(TAG, e.toString());
+			return null;
+		}
+	}
+
+	/**
+	 * creates a 128bit salted aes key
+	 *
+	 * @param key encoded input key
+	 * @return aes 128 bit salted key
+	 * @throws NoSuchAlgorithmException
+	 * @throws UnsupportedEncodingException
+	 * @throws InvalidKeySpecException
+	 */
+	private SecretKey getSecretKey(char[] key) throws
+			NoSuchAlgorithmException,
+			UnsupportedEncodingException,
+			InvalidKeySpecException
+	{
+		SecretKeyFactory factory;
+		factory = SecretKeyFactory.getInstance(mSecretKeyType);
+
+		KeySpec spec = new PBEKeySpec(key,
+				mSalt.getBytes(mCharsetName),
+				mIterationCount,
+				mKeyLength);
+
+		SecretKey tmp = factory.generateSecret(spec);
+		return new SecretKeySpec(tmp.getEncoded(), mAlgorithm);
+	}
+
+	/**
+	 * takes in a simple string and performs an sha1 hash
+	 * that is 128 bits long...we then base64 encode it
+	 * and return the char array
+	 *
+	 * @param key simple inputted string
+	 * @return sha1 base64 encoded representation
+	 * @throws UnsupportedEncodingException
+	 * @throws NoSuchAlgorithmException
+	 */
+	private char[] hashTheKey(String key) throws UnsupportedEncodingException, NoSuchAlgorithmException
+	{
+		MessageDigest md = MessageDigest.getInstance("SHA1");
+		md.update(key.getBytes(mCharsetName));
+		return Base64.encodeToString(md.digest(), Base64.NO_PADDING).toCharArray();
+	}
+
+	//region getters and setters
+
+	/**
+	 * @return the charset name
+	 */
+	public String getCharsetName()
+	{
+		return mCharsetName;
+	}
+
+	/**
+	 * @param charsetName the new charset name
+	 */
+	public void setCharsetName(String charsetName)
+	{
+		mCharsetName = charsetName;
+	}
+
+	/**
+	 * @return the mAlgorithm used
+	 */
+	public String getAlgorithm()
+	{
+		return mAlgorithm;
+	}
+
+	/**
+	 * @param algorithm the mAlgorithm to be used
+	 */
+	public void setAlgorithm(String algorithm)
+	{
+		mAlgorithm = algorithm;
+	}
+
+	/**
+	 * @return the Base 64 mode
+	 */
+	public int getBase64Mode()
+	{
+		return mBase64Mode;
+	}
+
+	/**
+	 * @param base64Mode set the base 64 mode
+	 */
+	public void setBase64Mode(int base64Mode)
+	{
+		mBase64Mode = base64Mode;
+	}
+
+	/**
+	 * @return the type of aes key that will be created
+	 */
+	public String getSecretKeyType()
+	{
+		return mSecretKeyType;
+	}
+
+	/**
+	 * @param secretKeyType the type of aes key that will be created//
+	 */
+	public void setSecretKeyType(String secretKeyType)
+	{
+		mSecretKeyType = secretKeyType;
+	}
+
+	/**
+	 * @return the value used for salting
+	 */
+	public String getSalt()
+	{
+		return mSalt;
+	}
+
+	/**
+	 * @param salt the value used for salting.
+	 */
+	public void setSalt(String salt)
+	{
+		mSalt = salt;
+	}
+
+	/**
+	 * @return the length of key
+	 */
+	public int getKeyLength()
+	{
+		return mKeyLength;
+	}
+
+	/**
+	 * @param keyLength the length of key
+	 */
+	public void setKeyLength(int keyLength)
+	{
+		mKeyLength = keyLength;
+	}
+
+	/**
+	 * @return the number of times the password is hashed
+	 */
+	public int getIterationCount()
+	{
+		return mIterationCount;
+	}
+
+	/**
+	 * @param iterationCount the number of times the password is hashed
+	 */
+	public void setIterationCount(int iterationCount)
+	{
+		mIterationCount = iterationCount;
+	}
+
+	//endregion
+
+}
---
+++ JavaLibs/oshelper/src/main/java/com/denizensoft/oshelper/MsgTarget.java
@@ -0,0 +1,347 @@
+package com.denizensoft.oshelper;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MsgTarget extends Handler
+{
+	static final public int N_APP_MESSAGE			= 0xDEADBEEF;
+	static final public int N_MSG_COMMAND 			= ( N_APP_MESSAGE + 0 );
+	static final public int N_MSG_REQUEST 			= ( N_APP_MESSAGE + 1 );
+	static final public int N_MSG_NOTIFY 			= ( N_APP_MESSAGE + 2 );
+	static final public int N_MSG_TOKEN 			= ( N_APP_MESSAGE + 3 );
+
+	static final public int N_COMMAND = ( 1024 );
+	static final public int N_COMMAND_TRACK			= ( N_COMMAND + 1 );
+	static final public int N_COMMAND_DISMISS		= ( N_COMMAND + 2 );
+	static final public int N_COMMAND_QUIT			= ( N_COMMAND + 2 );
+
+	static final public int N_COMMAND_USER 			= ( N_COMMAND + 256 );
+
+	static final public int N_NOTIFY				= ( N_COMMAND + 1024 );
+
+	static final public int N_NOTIFY_USER			= (N_NOTIFY + 256 );
+
+	static final public int N_RC_ERROR				= -1;
+	static final public int N_RC_OK					= 0;
+	static final public int N_RC_WARNING			= 1;
+	static final public int N_RC_WARNING_NOTFOUND	= 2;
+	static final public int N_RC_USER_CANCELLED		= 3;
+
+	static private enum StateCode
+	{
+		IDLE,
+		REPLY_PENDING,
+		REPLY_READY
+	}
+
+	static public enum ReplyCode
+	{
+		CRITICAL_ERROR,
+		SUCCESS_REQUEST,
+		WARNING_MESSAGE,
+		WARNING_NOTFOUND,
+		USER_CANCELLED
+	}
+
+	public interface HookInterface
+	{
+		public void cleanupRequestHook();
+
+		public void commandHook(int nCommand);
+
+		public void tokenHook(String stToken, Message msg);
+
+		public void fatalRequestErrorHook(String stFatalError);
+
+		public void invokeRequestHook(JSONObject jsonRequest, final JSONObject jsonReply);
+
+		MsgTarget messageTarget();
+
+		public void notificationHook(int nNotify);
+
+		public boolean otherMessageHook(Message msg);
+
+	};
+
+	private StateCode RequestState = StateCode.IDLE;
+
+	private Object mRequestMutex = new Object();
+
+	private JSONObject mPendingReply = null;
+
+	protected HookInterface mHookInterface = null;
+
+	public void sendReply(ReplyCode replyCode, String stMessage)
+	{
+		if(mPendingReply == null)
+			return;
+
+		if(RequestState != StateCode.REPLY_PENDING)
+			return;
+
+		try
+		{
+			synchronized(mPendingReply)
+			{
+				switch(replyCode)
+				{
+					case CRITICAL_ERROR:
+					{
+						mPendingReply.put("$rc",N_RC_ERROR);
+
+						if(stMessage == null)
+							stMessage = "Operation caused a general error!";
+
+						if(!mPendingReply.has("$error"))
+							mPendingReply.put("$error",stMessage);
+					}
+					break;
+
+					case SUCCESS_REQUEST:
+					{
+						mPendingReply.put("$rc",N_RC_OK);
+
+						if(stMessage != null && !mPendingReply.has("$success"))
+							mPendingReply.put("$success",stMessage);
+					}
+					break;
+
+					case WARNING_MESSAGE:
+					{
+						mPendingReply.put("$rc",N_RC_WARNING);
+
+						if(stMessage != null  && !mPendingReply.has("$warning"))
+							mPendingReply.put("$warning",stMessage);
+					}
+					break;
+
+					case WARNING_NOTFOUND:
+					{
+						mPendingReply.put("$rc", N_RC_WARNING_NOTFOUND);
+
+						if(stMessage == null)
+							stMessage = "Operation caused a NOTFOUND warning!";
+
+						if(!mPendingReply.has("$warning"))
+							mPendingReply.put("$warning",stMessage);
+					}
+					break;
+
+					case USER_CANCELLED:
+					{
+						mPendingReply.put("$rc", N_RC_USER_CANCELLED);
+					}
+					break;
+
+				}
+
+				RequestState = StateCode.REPLY_READY;
+				mPendingReply.notifyAll();
+				mPendingReply = null;
+				mHookInterface.cleanupRequestHook();
+			}
+		}
+		catch(JSONException e)
+		{
+			mHookInterface.fatalRequestErrorHook(String.format("JSON exception: %s",e.getMessage()));
+		}
+	}
+
+	@Override
+	public void handleMessage(Message msg)
+	{
+		switch(msg.what)
+		{
+			case N_MSG_COMMAND:
+			{
+				if(mHookInterface != null)
+					mHookInterface.commandHook(msg.arg1);
+			}
+			break;
+
+			case N_MSG_REQUEST:
+			{
+				try
+				{
+					mPendingReply = (JSONObject)msg.obj;
+
+					synchronized(mPendingReply)
+					{
+						RequestState = StateCode.REPLY_PENDING;
+
+						// Don't monkey with this! As the request wait state can be set elsewhere!
+						// while the handler is in progress!
+						//
+						if(mHookInterface == null)
+						{
+							mPendingReply.put("$rc",N_RC_ERROR);
+							mPendingReply.put("$error","No handler was found for the operation!");
+
+							RequestState = StateCode.REPLY_READY;
+							mPendingReply.notifyAll();
+							mPendingReply = null;
+						}
+						else
+						{
+							JSONObject jsonRequest = new JSONObject(msg.getData().getString("$request"));
+
+							mHookInterface.invokeRequestHook(jsonRequest, mPendingReply);
+
+							if(RequestState == StateCode.REPLY_PENDING)
+								Log.d("MsgTarget", "Reply is still pending after start hook...");
+						}
+					}
+				}
+				catch(JSONException e)
+				{
+					mHookInterface.fatalRequestErrorHook(String.format("JSON Exception: %s",e.getMessage()));
+					return;
+				}
+			}
+			break;
+
+			case N_MSG_NOTIFY:
+			{
+				if(mHookInterface != null)
+					mHookInterface.notificationHook(msg.arg1);
+			}
+			break;
+
+			case N_MSG_TOKEN:
+			{
+				if(mHookInterface != null)
+				{
+					String stToken = msg.getData().getString("_msg_token");
+
+					if(stToken != null)
+						mHookInterface.tokenHook(stToken,msg);
+				}
+			}
+			break;
+
+			default:
+			{
+				if(!mHookInterface.otherMessageHook(msg))
+					super.handleMessage(msg);
+			}
+			break;
+
+		}
+	}
+
+	public void sendCommand(int nCommand,Bundle args)
+	{
+		Message msg = obtainMessage();
+
+		msg.what = N_MSG_COMMAND;
+		msg.arg1 = nCommand;
+
+		msg.setData(args);
+
+		sendMessage(msg);
+	}
+
+	public void sendNotify(int nNotify,Bundle args)
+	{
+		Message msg = obtainMessage();
+
+		msg.what = N_MSG_NOTIFY;
+		msg.arg1 = nNotify;
+
+		msg.setData(args);
+
+		sendMessage(msg);
+	}
+
+	public JSONObject sendRequest(JSONObject jsonRequest)
+	{
+		return sendRequest(jsonRequest.toString());
+	}
+
+	public JSONObject sendRequest(String stJSON)
+	{
+		Message msg = obtainMessage();
+
+		JSONObject jsReply = new JSONObject();
+
+		msg.what = N_MSG_REQUEST;
+		msg.setData(new Bundle());
+		msg.getData().putString("$request",stJSON);
+		msg.obj = jsReply;
+
+		if(this.getLooper().getThread().getId() == Thread.currentThread().getId())
+			mHookInterface.fatalRequestErrorHook("MsgTarget: Requesting thread id matches the handler looper id!");
+
+		synchronized(mRequestMutex)
+		{
+			RequestState = StateCode.REPLY_PENDING;
+
+			synchronized(jsReply)
+			{
+				sendMessage(msg);
+
+				Log.d("MsgTarget: " + Thread.currentThread().getId(), "Request posted, starting wait....");
+
+				while(RequestState == StateCode.REPLY_PENDING)
+				{
+					try
+					{
+						jsReply.wait();
+
+						Log.d("MsgTarget: "+Thread.currentThread().getId(),"Request wait completed....");
+					}
+					catch(InterruptedException e)
+					{
+						Log.d("MsgTarget: "+Thread.currentThread().getId(),"Request wait, interrupted....");
+					}
+				}
+			}
+
+			RequestState = StateCode.IDLE;
+		}
+		return jsReply;
+	}
+
+	public void sendToken(String stToken,Bundle args)
+	{
+		Message msg = obtainMessage();
+
+		msg.what = N_MSG_TOKEN;
+		msg.arg1 = 0;
+		
+		if(args == null)
+			args = new Bundle();
+
+		args.putString("_msg_token",stToken);
+
+		msg.setData(args);
+
+		sendMessage(msg);
+	}
+
+	public void setTargetInterface(HookInterface hookInterface)
+	{
+		mHookInterface = hookInterface;
+	}
+
+	public MsgTarget()
+	{
+	}
+
+	public MsgTarget(Looper looper)
+	{
+		super(looper);
+	}
+
+	public MsgTarget(HookInterface hookInterface)
+	{
+		mHookInterface = hookInterface;
+	}
+}
