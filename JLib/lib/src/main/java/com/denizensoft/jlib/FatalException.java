d06c057ec5c38ab937f4e7f206b38c8ab26b1009
Steven Marsh <sjm@tiger.quarknet>
2015-12-28 16:45:29 -0600

    oops, almost missed this..

--- JLib/lib/src/main/java/com/denizensoft/jlib/FatalException.java
+++ JLib/lib/src/main/java/com/denizensoft/jlib/FatalException.java
@@ -3,6 +3,24 @@ package com.denizensoft.jlib;
 /**
  * Created by sjm on 12/28/15.
  */
-public class FatalException
+public class FatalException extends LibException
 {
+	public FatalException()
+	{
+	}
+
+	public FatalException(String message)
+	{
+		super(message);
+	}
+
+	public FatalException(Throwable cause)
+	{
+		super(cause);
+	}
+
+	public FatalException(String message, Throwable cause)
+	{
+		super(message, cause);
+	}
 }
